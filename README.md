2e446e21b82b6a65a8b5a24a9867b86b78fbd9aa
dvincente <d.vincente@crestfi.io>
2024-12-09 09:31:35 +0800

    updated the deploy logic

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.ts
+```
+
+Compile, Deploy and Verify:
+
+```shell
+npx hardhat compile
+npx hardhat ignition deploy ./ignition/modules/ZombieOwnership.ts --network sepolia --verify
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,33 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+
+// 0x3751e0E7Fb1a67A2a4dEE0f5C2d142B357e4BCb7  (Account 1)
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+export default config;
