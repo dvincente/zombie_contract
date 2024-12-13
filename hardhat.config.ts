c8f8a840f1f3837bc9b7c2438d75cc3baf55d1df
dvincente <d.vincente@aol.com>
2024-12-13 16:07:19 +0800

    fixed hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,6 @@ import "@nomicfoundation/hardhat-toolbox";
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
-// 0x3751e0E7Fb1a67A2a4dEE0f5C2d142B357e4BCb7  (Account 1)
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 const MAINNET_PRIVATE_KEY = vars.get("MAINNET_PRIVATE_KEY");
 
